f10b03d0f79bc31ed00df757fe3b6bfde538698f
Nikita <herndlhofer@iclooud.com>
2017-03-27 21:21:05 +0300

    quite ready 6, need a bit more stuff to do

--- UI/somefile.js
+++ UI/somefile.js
@@ -1,5 +1,13 @@
-var user = "Ivan Ivanov";
-
+var user = "";
+var users = [{
+	"name" :"admin",
+	"password" : "17534" //123
+},
+{
+	"name":"moderator",
+	"password":"17514" //111
+}];
+"use strict";
 var articles =  [
 	{
 		id: 1,
@@ -178,6 +186,7 @@ var articles =  [
 		content:"orem ipsum dolor sit amet, consectetur adipiscing elit. Donec id arcu ac est fringilla laoreet. Curabitur ut faucibus massa. Mauris posuere, nibh sit amet pharetra aliquet, purus neque pulvinar ex, vitae egestas felis metus vitae augue.",
 		tags :["nice", "good", "health", "dog"]
 	},
+
 ];
 	function getArticles (skip, top, filterConfig){
 	skip = skip||0;
@@ -205,12 +214,15 @@ function getArticle(idString) {
 function valdiateArticle(object){
 	if (   object.id != null 
 		&& object. title != null 
+		&& object.title != ""
 		&& object.title.length < 100
 		&& object.summary.length < 200
 		&& object.summary != null 
+		&& object.summary != ""
 		&& object.createdAt != null 
 		&& object.author != null 
-		&& object.content != null)
+		&& object.content != null
+		&& object.content != "")
 		return true;
 	else return false;
 }
@@ -240,11 +252,11 @@ function addArticleToData (object){
 		return false;
 	for (var i = 0; i < articles.length; i++) {
 		if (object.id == articles[i].id){
-			object.id = articles[articles.length-1].id + 1;
+			object.id = articles.length+1;
 			break;
 			}
 		}
-		articles.push(object);
+		articles.unshift(object);
 		console.log(articles);
 		return true;
 }
@@ -325,9 +337,113 @@ var articleRenderer = ( function() {
         deleteArticleFromDOM : deleteArticleFromDOM
     };
 }());
+var userRenderer = (function() {
+	function drawUser(){
+		if (user == "")
+		{
+			var DOMUser = document.querySelector('#user-info');
+			DOMUser.innerHTML='Please, Log In';
+			var addButton = document.getElementById("add-button");
+			addButton.setAttribute("hidden", null)
+			addButton = document.getElementById("logout");
+			addButton.setAttribute("hidden", null)
+			addButton = document.getElementById("log-in")
+			addButton.removeAttribute("hidden", null)
+			document.getElementById("name").removeAttribute("hidden")
+			document.getElementById("password").removeAttribute("hidden")
+		}
+		else{
+			var DOMUser = document.querySelector('#user-info');
+			DOMUser.innerHTML='Logged in as ' + user;
+			var addButton = document.getElementById("add-button");
+			addButton.removeAttribute("hidden")
+			addButton = document.getElementById("logout");
+			addButton.removeAttribute("hidden", null)
+			addButton = document.getElementById("log-in")
+			addButton.setAttribute("hidden", null)
+			document.getElementById("name").setAttribute("hidden", null)
+			document.getElementById("password").setAttribute("hidden", null)
+		}
+	}
+	return {
+		drawUser: drawUser
+	}
+}());
+var eventRenderer = (function(){
+	function init(){
+		var addButton = document.getElementById("add-button");
+		addButton.addEventListener("click", addForm);
+		var exitButton = document.getElementById("logout");
+		exitButton.addEventListener("click", exitAcc);
+		var loginButton = document.getElementById("log-in");
+		loginButton.addEventListener("click", enterAcc)
+	}
+	function enterAcc(){
+		var tmpuser = document.getElementById("name").value;
+		var tmppass = document.getElementById("password").value;
+		var key = parseInt("0100010000000101", 2);
+		tmppass = tmppass^key;
+		tmpuser = {"name": tmpuser,
+					"password": tmppass};
+					function callback(element, index, array){
+			if (tmpuser.name != null && tmpuser.name != undefined && tmpuser.name != array[index].name)
+				return false;
+			if (tmpuser.password != null && tmpuser.password != undefined && tmpuser.password != array[index].password)
+				return false;
+			return true;
+		}
+		if (users.find(callback)){
+			user = tmpuser.name;
+		}
+		else {
+			alert("No such username and password combination found!")
+			user = "";
+		}
+		userRenderer.drawUser();
+	}
+	function exitAcc(){
+		user = "";
+		userRenderer.drawUser();
+	}
+	function addForm(){
+		document.getElementById("logout").setAttribute("hidden", null);
+		articleRenderer.removeArticles();
+		document.querySelector(".content").innerHTML = ' <div class="title-tags"> <div class ="title-editor">  <h1 class ="title-editor-title">Add a Title!</h1><textarea maxlength=100 class ="title-editor-input"></textarea> </div><div class ="tag-editor"> <h1 class ="tag-editor-title">Add tags!</h1><textarea placeholder="Note, that the tags must be divided by a comma" class ="tag-editor-input"></textarea >  </div></div><div class ="summary-editor"><h1 class ="summary-editor-title">Add a summary!</h1> <textarea maxlength=199 rows=4 class ="summary-editor-input"></textarea >   </div>       <div class ="content-editor">           <h1 class ="content-editor-title">Add content!</h1>   <textarea rows=8 class ="content-editor-input"></textarea > </div> </div>'
+				document.querySelector(".control-units").innerHTML = '<ul class ="control-units" >  <button id="add-save-button" >Добавить новость</button> <button id="cancel-button"  onclick="location.href=\'index.html\'" >cancel</button> '
+				var Button = document.getElementById("add-save-button");
+				Button.addEventListener("click", saveForm);
+				console.log("add news");
+	}
+	function saveForm(){
+				var content = document.querySelector(".content");
+				var article = {"id":articles.length+1,
+				"title": content.querySelector(".title-editor-input").value,
+				"tags": content.querySelector(".tag-editor-input").value.split(","),
+				"summary": content.querySelector(".summary-editor-input").value, 
+				"content": content.querySelector(".content-editor-input").value, 
+				"createdAt": new Date(),
+				"author": user }
+		if (!addArticle(article)){
+			alert("your form isnt valid, check the input!");
+			return;
+		}
+		else{
+			document.querySelector(".content").innerHTML = '<template id="template-article-list-item"><div class="article-list-item" data-id="id"><h3 class="article-list-item-title"> Заголовок статьи </h3><p class="article-list-item-summary"></p><div class="article-list-item-meta"> <ul class="article-list-item-tags"> </ul><p class="item-author"> Author: <span class="article-list-item-author"></span></p><span>date: <span class="article-list-item-date"></span></div></div></template>'
+			document.querySelector(".control-units").innerHTML = ' <a href = "#"> <img name="left" alt="Left" src="https://github.com/NikitHern/UniversityRepo/blob/master/mockups/strelkavlevo.png?raw=true"> </a> <button id="add-button">Добавить новость</button>   <button id="log-in" hidden >Log in!</button><a href= "#"><img name="right" alt="Right" src="https://github.com/NikitHern/UniversityRepo/blob/master/mockups/strelkavlevo.png?raw=true"> </a>'
+			startApp();
+			return;
+		}
+		console.log("saved");
+	}
+	return {
+		init: init
+	};
+}());
 document.addEventListener('DOMContentLoaded', startApp);
 function startApp() {
+	userRenderer.drawUser();
 	 articleRenderer.init();
+	 eventRenderer.init();
 	 renderArticles();
 }
 function renderArticles(skip, top) {
